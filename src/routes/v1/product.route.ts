88633350d858e2affbd1696411ecd0ae21d4140f
zakia <zakiakhanum105@gmail.com>
2025-06-05 13:08:19 +0500

    code for google sheets added

--- src/routes/v1/product.route.ts
+++ src/routes/v1/product.route.ts
@@ -76,7 +76,7 @@ const pool = new Pool({
       adjusted_price, 
       is_product_approve, 
       product_status, 
-      user
+      "user"
     )
     VALUES (
       $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23
@@ -104,7 +104,7 @@ const pool = new Pool({
       adjusted_price = EXCLUDED.adjusted_price,
       is_product_approve = EXCLUDED.is_product_approve,
       product_status = EXCLUDED.product_status,
-      user = EXCLUDED.user
+      "user" = EXCLUDED."user"
     RETURNING website_url`;
   
         
